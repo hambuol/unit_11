21fc1fd5e513bdf81e8adab6cb3b16cfa811e737
hambuol <oliver.hamburger@ssfs.org>
2016-11-15 20:23:22 -0500

    added stufff

--- ball.py
+++ ball.py
@@ -1,4 +1,4 @@
-import pygame
+import pygame,sys
 import random
 
 class Ball(pygame.sprite.Sprite):
@@ -10,23 +10,31 @@ class Ball(pygame.sprite.Sprite):
         self.screen = screen
         self.speedx = 6
         self.speedy = 4
+        self.vx = random.randint(1,3)
+        self.vy = random.randint(1,3)
         self.rect = self.image.get_rect()
-
     def update(self):
         self.rect.left += self.speedx
         self.rect.top += self.speedy
         if self.rect.right > self.screen.get_width() or self.rect.left < 0:
             self.speedx = -self.speedx
-        if self.rect.bottom > self.screen.get_height() or self.rect.top < 0:
+        if self.rect.top > self.screen.get_height() or self.rect.top < 0:
             self.speedy = -self.speedy
+        if self.rect.bottom > self.screen.get_height():
+            pygame.quit()
+            sys.exit()
+
+
 
     def collide(self, spriteGroup):
         if pygame.sprite.spritecollide(self, spriteGroup, True):
-            self.speedy = -self.speedy
-            self.speedx = -self.speedx
+            self.speedy = -self.speedy - self.vx
+            self.speedx = -self.speedx - self.vy
+
+
 
     def collide2(self, spriteGroup):
         if pygame.sprite.spritecollide(self, spriteGroup, False):
-            self.speedy = -self.speedy
-            self.speedx = -self.speedx
+            self.speedy = -self.speedy - self.vx
+            self.speedx = -self.speedx - self.vy
 
--- breakout.py
+++ breakout.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 import brick
 import paddle
 import ball
-import random
+
 
 def main():
     pygame.init()
@@ -16,7 +16,7 @@ def main():
     BRICK_Y_OFFSET = 70
     BRICK_WIDTH =  (APPLICATION_WIDTH - (BRICKS_PER_ROW + 1) * BRICK_SEP) / BRICKS_PER_ROW
     NUM_TURNS = 3
-    NUM_ROWS = 2
+    num_rows = 2
 
     #Sets up the colors
     RED = (255, 0, 0)
@@ -27,7 +27,6 @@ def main():
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     colors = [RED, ORANGE, YELLOW, GREEN, CYAN,]
-    font = pygame.font.Font(None, 36)
     mainsurface = pygame.display.set_mode((APPLICATION_WIDTH, APPLICATION_HEIGHT), 0, 32)
     pygame.display.set_caption("Breakout")
     mainsurface.fill(WHITE)
@@ -35,40 +34,46 @@ def main():
     padGroup = pygame.sprite.Group()
     xpos = BRICK_SEP
     ypos = 100
-    for color in colors:
-            for q in range(NUM_ROWS):
-                for x in range(BRICKS_PER_ROW):
-                    mybrick = brick.Brick(BRICK_WIDTH, color)
-                    mybrick.rect.topleft = (xpos, ypos)
-                    bricksGroup.add(mybrick)
-                    mainsurface.blit(mybrick.image,mybrick.rect)
-                    xpos += BRICK_WIDTH + BRICK_SEP
-                ypos += BRICK_SEP * 3
-                xpos = BRICK_SEP
-
+    def brik(xpos, ypos):
+        for color in colors:
+                for q in range(num_rows):
+                    for x in range(BRICKS_PER_ROW):
+                        mybrick = brick.Brick(BRICK_WIDTH, color)
+                        mybrick.rect.topleft = (xpos, ypos)
+                        bricksGroup.add(mybrick)
+                        mainsurface.blit(mybrick.image,mybrick.rect)
+                        xpos += BRICK_WIDTH + BRICK_SEP
+                    ypos += BRICK_SEP * 3
+                    xpos = BRICK_SEP
+    brik(xpos,ypos)
     mypaddle = paddle.Paddle(BLACK)
     padGroup.add(mypaddle)
     mypaddle.rect.topleft = (100,550)
     myball = ball.Ball(mainsurface, BLACK)
-    myball.rect.topleft = (100 ,400)
+    myball.rect.topleft = (125 ,400)
 
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+
         clock = pygame.time.Clock()
-        clock.tick(60)
+        clock.tick(50)
         mainsurface.fill(WHITE)
         ppos = pygame.mouse.get_pos()
-
         if myball.collide(bricksGroup):
             bricksGroup.remove()
+
+        # attempt at adding row and readding briks to screnn to give effect of levels to game
+        if bricksGroup == 0:
+            num_rows += 1
+            brik(xpos,ypos)
 
         for apad in padGroup:
             myball.collide2(padGroup)
             padGroup.update(apad)
-
 
         mypaddle.rect.topleft = (ppos[0], 550)
 
@@ -76,9 +81,7 @@ def main():
         myball.update()
         for mybrick in bricksGroup:
             mainsurface.blit(mybrick.image, mybrick.rect)
-        if myball.rect.topleft[1] == 580:
-            pygame.quit()
-            sys.exit()
+
 
         mainsurface.blit(mypaddle.image, mypaddle.rect)
         mainsurface.blit(myball.image, myball.rect)
